92e3703c96ea805e9e824f582ee0567fa69611e4
Yusuke Ito <novi.mad@gmail.com>
2015-11-06 19:36:51 +0900

    add data view controller for users

--- MySQLDemo/MySQLDemo/DatabaseViewController.swift
+++ MySQLDemo/MySQLDemo/DatabaseViewController.swift
@@ -9,20 +9,18 @@
 import Cocoa
 import MySQL
 
-class ViewController: NSViewController {
+class DatabaseViewController: NSViewController {
 
     
     @IBOutlet weak var hostField: NSTextField!
     @IBOutlet weak var portField: NSTextField!
     @IBOutlet weak var userField: NSTextField!
     @IBOutlet weak var passwordField: NSTextField!
+    @IBOutlet weak var databaseComboBox: NSComboBox!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-
         // Do any additional setup after loading the view.
     }
 
@@ -35,15 +33,11 @@ class ViewController: NSViewController {
     
     @IBAction func connectTapped(sender: AnyObject) {
         
-        let info = Database.ConnectionInfo(host: hostField.stringValue, port: Int(portField.stringValue) ?? 0, userName: userField.stringValue, password: passwordField.stringValue, database: "test")
+        let info = Database.ConnectionInfo(host: hostField.stringValue, port: Int(portField.stringValue) ?? 0, userName: userField.stringValue, password: passwordField.stringValue, database: databaseComboBox.stringValue)
         let database = Database(info: info)
         do {
             let conn = try database.getConnection()
-            
-            let rows: [Row.User] = try conn.query("SELECT * FROM users", args:[])
-            for row in rows {
-                print("\(row.id) : \(row.userName) \(row.age)")
-            }
+            self.performSegueWithIdentifier("Connection", sender: conn)
             
         } catch (let e) {
             print("\(e as ErrorType)")
@@ -53,6 +47,12 @@ class ViewController: NSViewController {
             self.presentError(err)
         }
         
+    }
+    
+    override func prepareForSegue(segue: NSStoryboardSegue, sender: AnyObject?) {
+        if let dst = segue.destinationController as? ConnectionViewController, let conn = sender as? Connection {
+            dst.connection = conn
+        }
     }
 
 
