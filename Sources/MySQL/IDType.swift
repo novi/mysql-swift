2f273770effd4e87951c61b5a758f197fd38e904
Yusuke Ito <novi.mad@gmail.com>
2018-01-12 18:13:39 +0900

    Decodable support for AutoincrementID and IDType

--- Sources/MySQL/IDType.swift
+++ Sources/MySQL/IDType.swift
@@ -8,8 +8,8 @@
 
 import SQLFormatter
 
-public protocol IDType: SQLStringDecodable, QueryParameter, Hashable {
-    associatedtype T: SQLStringDecodable, QueryParameter, Hashable
+public protocol IDType: SQLStringDecodable, QueryParameter, Hashable, Decodable {
+    associatedtype T: SQLStringDecodable, QueryParameter, Hashable, Decodable
     var id: T { get }
     init(_ id: T)
 }
@@ -29,4 +29,30 @@ public extension IDType {
 
 public func ==<T: IDType>(lhs: T, rhs: T) -> Bool {
     return lhs.id == rhs.id
+}
+
+// MARK: Decodable type
+
+extension IDType where T == Int {
+    public init(from decoder: Decoder) throws {
+        self.init(try decoder.singleValueContainer().decode(Int.self))
+    }
+}
+
+extension IDType where T == Int64 {
+    public init(from decoder: Decoder) throws {
+        self.init(try decoder.singleValueContainer().decode(Int64.self))
+    }
+}
+
+extension IDType where T == UInt {
+    public init(from decoder: Decoder) throws {
+        self.init(try decoder.singleValueContainer().decode(UInt.self))
+    }
+}
+
+extension IDType where T == UInt64 {
+    public init(from decoder: Decoder) throws {
+        self.init(try decoder.singleValueContainer().decode(UInt64.self))
+    }
 }
